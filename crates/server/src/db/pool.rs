c0c2738c31d61f68205e935a28c34cd97ca8acfc
Chrislearn Young <chris@acroidea.com>
2024-11-19 09:10:37 +0800

    remove unused `ConnectionConfig`

--- crates/server/src/db/pool.rs
+++ crates/server/src/db/pool.rs
@@ -69,31 +69,6 @@ impl Deref for DieselPool {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct ConnectionConfig {
-    pub statement_timeout: Duration,
-    pub read_only: bool,
-}
-
-impl CustomizeConnection<PgConnection, r2d2::Error> for ConnectionConfig {
-    fn on_acquire(&self, conn: &mut PgConnection) -> Result<(), r2d2::Error> {
-        use diesel::sql_query;
-
-        sql_query(format!(
-            "SET statement_timeout = {}",
-            self.statement_timeout.as_millis()
-        ))
-        .execute(conn)
-        .map_err(r2d2::Error::QueryError)?;
-        if self.read_only {
-            sql_query("SET default_transaction_read_only = 't'")
-                .execute(conn)
-                .map_err(r2d2::Error::QueryError)?;
-        }
-        Ok(())
-    }
-}
-
 #[derive(Debug, Error)]
 pub enum PoolError {
     #[error(transparent)]
