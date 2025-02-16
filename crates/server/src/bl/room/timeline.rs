1b652707f3a75afc66d48b08860d71da5e3f64b3
Chrislearn Young <chris@acroidea.com>
2025-02-16 21:07:51 +0800

    wip

--- crates/server/src/bl/room/timeline.rs
+++ crates/server/src/bl/room/timeline.rs
@@ -649,6 +649,7 @@ pub fn create_hash_and_sign_event(
 /// Creates a new persisted data unit and adds it to a room.
 #[tracing::instrument]
 pub fn build_and_append_pdu(pdu_builder: PduBuilder, sender: &UserId, room_id: &RoomId) -> AppResult<PduEvent> {
+    println!("===============build_and_append_pdu");
     let (pdu, pdu_json) = create_hash_and_sign_event(pdu_builder, sender, room_id)?;
     let conf = crate::config();
     let admin_room = crate::room::resolve_local_alias(
