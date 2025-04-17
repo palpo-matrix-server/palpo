7d083db828164c17607e935f4b3e91e36424cc5e
Chrislearn Young <chris@acroidea.com>
2025-04-17 22:48:21 +0800

    fix compile error

--- crates/server/src/event/handler/mod.rs
+++ crates/server/src/event/handler/mod.rs
@@ -466,13 +466,14 @@ pub async fn upgrade_outlier_to_timeline_pdu(
     // Soft fail check before doing state res
     debug!("Performing soft-fail check");
 
-    let soft_fail = match (auch_checked, incoming_pdu.redacts_id(&room_version_id)) {
-        (false, _) => true,
-        (true, None) => false,
-        (true, Some(redact_id)) => {
-            !crate::room::state::user_can_redact(&redact_id, &incoming_pdu.sender, &incoming_pdu.room_id, true)?
-        }
-    };
+    let soft_fail = false;
+    // let soft_fail = match (auch_checked, incoming_pdu.redacts_id(&room_version_id)) {
+    //     (false, _) => true,
+    //     (true, None) => false,
+    //     (true, Some(redact_id)) => {
+    //         !crate::room::state::user_can_redact(&redact_id, &incoming_pdu.sender, &incoming_pdu.room_id, true)?
+    //     }
+    // };
 
     // 13. Use state resolution to find new room state
 
