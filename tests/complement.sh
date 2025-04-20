e351dc9ec7d9bc273f34a6165d8eea78cfc4904f
Chrislearn Young <chris@acroidea.com>
2025-04-20 08:13:06 +0800

    w

--- tests/complement.sh
+++ tests/complement.sh
@@ -53,7 +53,7 @@ env \
     -C "$COMPLEMENT_SRC" \
     COMPLEMENT_ALWAYS_PRINT_SERVER_LOGS=1 \
     COMPLEMENT_BASE_IMAGE="$TEST_IMAGE" \
-    go test -tags="palpo_blacklist" "$SKIPPED_COMPLEMENT_TESTS" -timeout 2h -run "TestRequestEncodingFails" -json "${test_packages[@]}"| tee "$LOG_FILE.jsonl"
+    go test -tags="palpo_blacklist" "$SKIPPED_COMPLEMENT_TESTS" -timeout 2h -run "^(TestKnocking/A_user_in_the_room_can_reject_a_knock)$" -json "${test_packages[@]}"| tee "$LOG_FILE.jsonl"
 set -o pipefail
 
 # Post-process the results into an easy-to-compare format
