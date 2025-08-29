8b9cde638ec0544523069a706eb036b4b3f084e7
Chrislearn Young <chris@acroidea.com>
2025-08-29 23:09:31 +0800

    w

--- crates/core/src/state/event_auth/mod.rs
+++ crates/core/src/state/event_auth/mod.rs
@@ -22,12 +22,12 @@ use crate::{
     state::events::{
         RoomCreateEvent, RoomJoinRulesEvent, RoomMemberEvent, RoomPowerLevelsEvent,
         RoomThirdPartyInviteEvent,
-        member::RoomMemberEventContent,
+        member::{RoomMemberEventOptionExt, RoomMemberEventContent},
         power_levels::{RoomPowerLevelsEventOptionExt, RoomPowerLevelsIntField},
     },
     state::{Event, StateError, StateResult},
     utils::RoomIdExt,
-};
+}; 
 
 /// Get the list of [relevant auth events] required to authorize the event of the given type.
 ///
@@ -783,7 +783,7 @@ where
     async fn user_membership(&self, user_id: &UserId) -> StateResult<MembershipState> {
         self(StateEventType::RoomMember, user_id.as_str().into())
             .await
-            .map(RoomMemberEvent::new)?
+            .map(RoomMemberEvent::new).ok()
             .membership()
     }
 
